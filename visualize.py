342de7fd1720e5cc50e219649bf298f826a99f02
Andrea Rosasco <andrearosasco.ar@gmail.com>
2023-04-05 12:22:46 +0000

    completed visualizer

--- visualize.py
+++ visualize.py
@@ -1,17 +1,64 @@
 import yarp
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
 
-yarp.Network.init()
+# Set up the plot
+fig, ax = plt.subplots()
+ax.set_ylim([-1.1, 1.1])
+ax.set_title('Line of sight vector')
 
-input_port = yarp.BufferedPortBottle()
-input_port.open("/GazeController/Visualizer/debug:i")
+line1, = ax.plot(np.zeros(100), color='blue', label='desired x')
+line2, = ax.plot(np.zeros(100), color='red', label='desired y')
+line3, = ax.plot(np.zeros(100), color='green', label='desired z')
 
-yarp.Network.connect("/GazeController/debug:o", "/GazeController/Visualizer/debug:i")
+line4, = ax.plot(np.zeros(100), color='blue', linestyle='dotted', label='actual x')
+line5, = ax.plot(np.zeros(100), color='red', linestyle='dotted', label='actual y')
+line6, = ax.plot(np.zeros(100), color='green', linestyle='dotted', label='actual z')
 
-while True:
+ax.legend()
+
+lines1 = [line1, line2, line3]
+lines2 = [line4, line5, line6]
+
+# Update the plot
+def update(data):
     bottle = input_port.read(False)
     if bottle is not None:
         point = bottle.get(0).asList()
         vector = bottle.get(1).asList()
 
-        print("Point:", point.toString())
-        print("Vector:", vector.toString())
+        if point is not None:
+
+            for i, line in enumerate(lines1):
+
+                x = np.roll(line.get_xydata()[:, 0], -1)
+                y = np.roll(line.get_xydata()[:, 1], -1)
+
+                x[-1] = x[-2] + 1
+                y[-1] = point.get(i).asFloat64()
+
+                line.set_ydata(y)
+
+        if vector is not None:
+            for i, line in enumerate(lines2):
+
+                x = np.roll(line.get_xydata()[:, 0], -1)
+                y = np.roll(line.get_xydata()[:, 1], -1)
+
+                x[-1] = x[-2] + 1
+                y[-1] = vector.get(i).asFloat64()
+
+                line.set_ydata(y)
+
+    return lines1, lines2
+
+# Animate the plot
+yarp.Network.init()
+
+input_port = yarp.BufferedPortBottle()
+input_port.open("/GazeController/Visualizer/debug:i")
+yarp.Network.connect("/GazeController/debug:o", "/GazeController/Visualizer/debug:i")
+
+ani = animation.FuncAnimation(fig, update)
+plt.show()
