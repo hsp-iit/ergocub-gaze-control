a93dd861451f2d6f2a70952231e7349fb01a8c7e
Andrea Rosasco <andrearosasco.ar@gmail.com>
2023-04-11 11:45:09 +0000

    start the gaze controller rpc server - implements the threft generated interface - starts gc on a different thread - accepts incoing calls that modifies gc gazing point

---
+++ src/server.cpp
@@ -0,0 +1,89 @@
+
+#include <thread>
+#include <yarp/os/Network.h>
+#include <yarp/os/RpcServer.h>
+#include <yarp/os/Property.h>
+#include "RPCServerInterface.h"
+#include "GazeControl.h"
+
+class RPCServer: RPCServerInterface{
+public:
+    GazeControl* gazeController;
+
+    RPCServer(GazeControl* gazeControl){
+        this->gazeController = gazeControl;
+    };
+
+    bool open()
+    {
+        this->yarp().attachAsServer(server_port);
+        if (!server_port.open("/Components/GazeController")) {
+            yError("Could not open ");
+            return false;
+        }
+        return true;
+    }
+
+    void close()
+    {
+        server_port.close();
+    }
+
+    bool look_at(const std::vector<double>& point){
+        std::vector<double> gaze(point);
+        Eigen::Map<Eigen::VectorXd> eigen_gaze(&gaze[0], 3);
+        this->gazeController->set_gaze(eigen_gaze);
+        return true;
+    }
+private:
+yarp::os::RpcServer server_port;
+};
+
+int main(int argc, char *argv[])
+{
+    
+    std::string config_file = "config.ini";
+    
+    if (argc == 2){
+        config_file = argv[1];
+    }
+        
+    yarp::os::Property prop;
+    prop.fromConfigFile(config_file);
+
+    std::string pathToURDF = prop.find("urdf").asString();
+
+    auto joints = prop.findGroup("joints");
+    std::vector<std::string> jointList;
+    for (int i=1; i < joints.size(); i++)
+        jointList.push_back(joints.get(i).asString());
+
+    
+    auto ports = prop.findGroup("ports");
+    std::vector<std::string> portList;
+    for (int i=1; i < ports.size(); i++)
+        portList.push_back(ports.get(i).asString());
+
+    double sample_time = prop.find("sample_time").asFloat64();
+
+    yarp::os::Network yarp;
+
+    GazeControl gazeController(pathToURDF, jointList, portList, sample_time);
+    gazeController.set_cartesian_gains(15);
+
+    gazeController.start();
+
+    RPCServer server(&gazeController);
+
+    if (!server.open()) {
+        return 1;
+    }
+
+    while (true) {
+        yInfo("Server running happily");
+        yarp::os::Time::delay(10);
+    }
+
+    server.close();
+    return 0;
+}
