6657cb1194c6eb8e12b5d4383738bce62ad6654c
Andrea Rosasco <andrearosasco.ar@gmail.com>
2023-04-28 14:52:51 +0000

    updated target frequency

--- src/test.cpp
+++ src/test.cpp
@@ -38,7 +38,7 @@ int main(int argc, char *argv[])
 
     gaze_controller.yarp().attachAsClient(client_port);
 
-    double f = 1;
+    double f = 2;
     double A = 0.1;
     double t = 0;
     // std::vector<double> root_to_camera{-0.092092 -0.2, 0.001294, 0.320022};  // iCub
@@ -50,7 +50,7 @@ int main(int argc, char *argv[])
         double y = std::cos(2 * M_PI * f * t) * A;
         double z = std::sin(2 * M_PI * f * t) * A;
 
-        std::vector<double> point{0.1, y, z};
+        std::vector<double> point{0.5, y, z};
 
         for(int i=0; i<3; i++){
             point[i] += root_to_camera[i];
